d4beb4c17bf7e0a43f44669bc4c2b66c309852c0
nakamura hiroki <nakamura2465@gmail.com>
2022-06-29 18:28:35 +0900

    ボディ部分を作成

---
+++ dist/assets/js/ambition/regular.js
@@ -0,0 +1,33 @@
+$(function() {
+  $(document).ready(function() {
+    window.scroll({top:0})
+    $("#profile, #scroll-induction").addClass('block-in');
+  });
+  /**
+   * スクロール時に要素がでたら表示される処理
+   */
+  $(window).on('scroll',function() {
+    $(".scroll-block").each(function() {
+      let scroll = $(window).scrollTop();
+      let blockPosition = $(this).offset().top;
+      let windowHeihgt = $(window).height();
+      if (scroll > blockPosition - windowHeihgt + 150) {
+        $(this).addClass("block-in");
+      }
+    });
+  });
+  /**
+   * マウスホイールでスクロール時のスクロール量を調整する
+   */
+  $('#scroll-induction, #interview-content-1, #interview-content-2, #interview-content-3, #interview-ambition, #other-ambition, #footer').inertiaScroll({
+    parent: $("#wrap"),
+    childDelta1	: 0.02
+  }); 
+});
+
+/**
+ * 他の社員の記事にとぶ
+ */
+const toOtherPerson = function(index) {
+  window.location.href=`interview0${index}.html`;
+};   
--- src/app.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery'
-
-import { regular } from './ambition/regular'
-
-$('#head').load('../../common/head/head.html')
-$('#header').load('../../common/header/header.html')
-$('#footer').load('../../common/footer/footer.html')
-
-regular()
